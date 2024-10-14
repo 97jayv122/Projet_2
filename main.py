4d93c2aca988ad3915097b339b716d464b61da85
97jayv122 <jay-v@live.fr>
2024-10-14 18:38:47 +0200

    ajout des éléments à extraire sur une page d'un livre

--- main.py
+++ main.py
@@ -7,34 +7,40 @@ def extraire(url):
     page = response.content
     soup = BeautifulSoup(page, "html.parser")
     titre_livre = soup.h1.string
-    produit_description = soup.p.string
-    print(produit_description)
+    produit_description = soup.find("p", class_=None)
+    produit_description = produit_description.string
+    categorie = soup.find("ul").find_all("li")[2].find("a").string
+    evaluation_avis = soup.find("p" ,class_="star-rating")
+    evaluation_avis = evaluation_avis.get("class")[1]
     elements_tableau = soup.find_all("tr")
-    return titre_livre, produit_description, elements_tableau
+    return evaluation_avis, categorie, titre_livre, produit_description, elements_tableau
     
 def transformer(element):
     titres_tableau = element.find("th")
     descriptions_tableau = element.find("td")
     return (titres_tableau.string, descriptions_tableau.string)
 
-def charger(page_product_url, titre_livre, produit_descrption, en_tete, donnees, ):
+def charger(page_product_url, titre_livre, produit_descrption, categorie, evaluation_avis, en_tete, donnees, ):
     with open("data.csv", "w", newline="") as fichier_csv:
         writer = csv.writer(fichier_csv, delimiter=',')
         writer.writerow(page_product_url)
         writer.writerow(titre_livre)
         writer.writerow(produit_descrption)
+        writer.writerow(categorie)
+        writer.writerow(evaluation_avis)
         writer.writerow(en_tete)
         for donnee in donnees:
             writer.writerow(donnee)
 def etl(url):
-    page_product_url =["page_product_url", url] 
-    
-    titre_livre, produit_description, elements_tableau = extraire(url)
-    titre_livre = ["livre", titre_livre]
-    produit_description = ["produit_description", produit_description]
+    page_product_url =["Page_product_url", url] 
+    evaluation_avis, categorie, titre_livre, produit_description, elements_tableau = extraire(url)
+    titre_livre = ["Livre", titre_livre]
+    produit_description = ["Produit_description", produit_description]
     produit_informations = [transformer(element) for element in elements_tableau]
-    en_tete = ["titres", "descriptions"]
-    charger(page_product_url, titre_livre, produit_description, en_tete, produit_informations)
+    categorie = ["Catégorie", categorie]
+    evaluation_avis = ["Evaluation_avis", evaluation_avis]
+    en_tete = ["Titres", "Descriptions"]
+    charger(page_product_url, titre_livre, produit_description, categorie, evaluation_avis, en_tete, produit_informations)
 
 if __name__ == "__main__" : 
     etl("https://books.toscrape.com/catalogue/scott-pilgrims-precious-little-life-scott-pilgrim-1_987/index.html")       
